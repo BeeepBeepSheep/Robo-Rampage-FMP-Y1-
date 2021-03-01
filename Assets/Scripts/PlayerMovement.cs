281782826b92a236fa98d2d433f28e082dfea5fe
BeeepBeepSheep <515291@chrome.escg.ac.uk>
2021-03-01 12:35:51 +0000

    fixed issue were default script was interfearing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,7 +15,7 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transoform.right * x + transform.forward * z;
+        Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
     }
