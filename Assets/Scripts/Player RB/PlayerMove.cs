42016cb3d50e52b6ba8d0dd7d40bd6b23be53876
BeeepBeepSheep <515291@chrome.escg.ac.uk>
2021-03-02 21:21:45 +0000

    added wallrunning parts to script (need to fix errors and add camera rotation)

--- Assets/Scripts/Player RB/PlayerMove.cs
+++ Assets/Scripts/Player RB/PlayerMove.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-
+    //rigidbody
     private Rigidbody rb;
 
+    //assignables
+    public Transform orientation;
+    public Transform playerCam;
+
+    //speed & jump
     public float moveSpeed;
     public float normalSpeed = 6f;
     public float jumpForce = 12f;
-    public float sprintSpeed = 2f;
+    public float sprintSpeed = 25f;
     public float crouchSpeed = 3f;
 
-    public LayerMask layerMask;
+    //ground checking
+    public LayerMask whatIsGround;
     public bool Grounded;
+
+    //wall wunning
+    public LayerMask whatIsWall;
+    public float wallrunForce, maxWallrunTime, maxWallSpeed;
+    bool isWallRight, isWallLeft;
+    bool isWallRunning;
+    public float maxCameraTilt, CameraTilt;
 
     void Start()
     {
@@ -26,41 +39,97 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
+
+        CheckForWall();
+        WallRunInput();
+
+
+        //walking
         float x = Input.GetAxisRaw("Horizontal") * moveSpeed;
         float y = Input.GetAxisRaw("Vertical") * moveSpeed;
+
+        Grounded = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - 1, transform.position.z), 0.4f, whatIsGround);
+
 
         Vector3 movePosition = transform.right * x + transform.forward * y;
         Vector3 newMovePosition = new Vector3(movePosition.x, rb.velocity.y, movePosition.z);
 
         rb.velocity = newMovePosition;
 
-        if (Input.GetKeyDown(KeyCode.Space) && Grounded)
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
-
-        Grounded = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - 1, transform.position.z), 0.4f, layerMask);
-
         //sprinting
-
-
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Grounded)
         {
-            moveSpeed =  moveSpeed * sprintSpeed;
+            moveSpeed = sprintSpeed;
         }
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            moveSpeed = moveSpeed * normalSpeed;
+            moveSpeed = normalSpeed;
         }
 
+        //jumping
 
+        if (Input.GetKeyDown(KeyCode.Space) && Grounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            if (!(Input.GetKeyDown(KeyCode.LeftShift)))
+            {
+                //need to wait a seccond before setting move speed back
+                moveSpeed = normalSpeed;
+            }
 
-        //if (!(Input.GetKeyDown(KeyCode.LeftShift)))
-        //{
-        //    moveSpeed = normalSpeed;
-        //}
+        }
+        //jump off walls
+        WallJump();
+    }
 
+    //wallrunning
+    private void WallRunInput()
+    {
+        if (WallRunInput.GetKey(KeyCode.D) && isWallRight) StartWallRun;
+        if (WallRunInput.GetKey(KeyCode.A) && isWallLeft) StartWallRun;
+    }
+    private void StartWallRun()
+    {
+        rb.useGravity = false;
+        isWallRunning = true;
 
+        if(rb.velocity.magnitude <= maxWallSpeed)
+        {
+            rb.AddForce(orientation.forward * wallrunForce * maxWallrunTime.deltaTime);
 
+            //make sure character sticks to the wall
+            if (isWallRight)
+                rb.AddForce(orietation.right * wallrunForce / 5 * maxWallrunTime.deltaTime);
+            else
+                rb.AddForce(-orietation.right * wallrunForce / 5 * maxWallrunTime.deltaTime);
+        }
+    }
+    private void CheckForWall()
+    {
+        isWallRight = Physics.Raycast(transform.position, orientation.right, 1f, whatIsWall);
+        isWallLeft = Physics.Raycast(transform.position, -orientation.right, 1f, whatIsWall);
+        //leave wall
+        if (!isWallLeft && !isWallRight) StopWallRun();
 
+    }
+    private void StopWallRun()
+    {
+        rb.useGravity = true;
+        isWallRunning = false;
+    }
+    private void WallJump()
+    {
+        if (isWallRunning)
+        {
+            //normal jump
+            if (isWallLeft && !WallRunInput.GetKey(HeyCode.D) || isWallRight && !isWallRight && !WallRunInput.GetKey(KeyCode.A))
+            {
+                rb.AddForce(Vector2.up * jumpForce * 1.5f);
+                rb.AddForce(normalVector * jumpForce * 0.5f);
+            }
+
+            //add more force
+            rb.AddForce(orientation.forward * jumpForce * 1f);
+        }    
     }
 }
