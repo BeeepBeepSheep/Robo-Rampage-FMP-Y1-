bd3f1391b04eb12f08e1aaacadd661c4338216da
515291 <515291@chrome.escg.ac.uk>
2021-03-25 11:40:12 +0000

    Gun particle effects

---
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+    public float damage = 34f;
+    public float range = 100f;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public float impactForce = 30f;
+
+    public Camera cam;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+            GameObject impactGameObject = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGameObject, 2f);
+        }
+    }
+}
