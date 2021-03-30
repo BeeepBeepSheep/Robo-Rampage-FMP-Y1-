d0df66edc35bc17d2a07ed11727bf98b7f56021d
515291 <515291@chrome.escg.ac.uk>
2021-03-30 10:21:44 +0100

    got ai to follow

---
+++ Assets/Scripts/Enemies/EnemieController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemieController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    Transform target;
+    NavMeshAgent agent;
+    void Start()
+    {
+        target = PlayerManeger.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, lookRadius);
+    }
+}
---
+++ Assets/Scripts/PlayerManeger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManeger : MonoBehaviour
+{
+    #region Singleton
+
+    public static PlayerManeger instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    public GameObject player;
+}
