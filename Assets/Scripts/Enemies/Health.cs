11283f0172c91cd1310227521eee44ceba6d6c37
515291 <515291@chrome.escg.ac.uk>
2021-04-01 12:34:23 +0100

    got robot textures working

--- Assets/Scripts/Enemies/Health.cs
+++ Assets/Scripts/Enemies/Health.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Health : MonoBehaviour
 {
     public float health = 100f;
@@ -9,13 +9,13 @@ public class Health : MonoBehaviour
     public void TakeDamage(float amount)
     {
         health -= amount;
-        if(health <= 0f)
+        if (health <= 0f)
         {
             Die();
         }
     }
     void Die()
     {
-        Destroy(gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float playerHealth = 100f;
+
+    public void TakeDamage(float amount)
+    {
+        playerHealth -= amount;
+        if (playerHealth <= 0f)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
