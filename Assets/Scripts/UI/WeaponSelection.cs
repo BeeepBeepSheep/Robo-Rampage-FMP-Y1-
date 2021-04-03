1a6043dde65d107a1e8d72802c0d9ffff4557649
BeeepBeepSheep <515291@chrome.escg.ac.uk>
2021-04-03 13:41:18 +0100

    got weapon switching

---
+++ Assets/Scripts/UI/WeaponSelection.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSelection : MonoBehaviour
+{
+    public GameObject AR;
+    public GameObject Minigun;
+    public GameObject Sniper;
+
+    public GameObject Pistol;
+    public GameObject ShotGun;
+    public GameObject Knife;
+
+    public void AR_Selection()
+    {
+        AR.SetActive(true);
+        Minigun.SetActive(false);
+        Sniper.SetActive(false);
+    }
+    public void Minigun_Selection()
+    {
+        AR.SetActive(false);
+        Minigun.SetActive(true);
+        Sniper.SetActive(false);
+    }
+    public void Sniper_Selection()
+    {
+        AR.SetActive(false);
+        Minigun.SetActive(false);
+        Sniper.SetActive(true);
+    }
+    public void Knife_Selection()
+    {
+        Pistol.SetActive(false);
+        ShotGun.SetActive(false);
+        Knife.SetActive(true);
+    }
+    public void Pistol_Selection()
+    {
+        Pistol.SetActive(true);
+        ShotGun.SetActive(false);
+        Knife.SetActive(false);
+    }
+    public void Shotgun_Selection()
+    {
+        Pistol.SetActive(false);
+        ShotGun.SetActive(true);
+        Knife.SetActive(false);
+    }
+}
