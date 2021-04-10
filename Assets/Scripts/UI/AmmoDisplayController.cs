472ba69253b8d90b80dfb9ee9b3e29d78f34e9b1
BeeepBeepSheep <515291@chrome.escg.ac.uk>
2021-04-10 22:40:14 +0100

    got ammo displays switching

---
+++ Assets/Scripts/UI/AmmoDisplayController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoDisplayController : MonoBehaviour
+{
+    public GameObject TertararyAmmoDisplay;
+    public GameObject MainAmmoDisplay;
+
+    public GameObject DaulWieldHolder;
+    void Update()
+    {
+        if (DaulWieldHolder.activeInHierarchy == true)
+        {
+            MainAmmoDisplay.SetActive(false);
+            TertararyAmmoDisplay.SetActive(true);
+        }
+        if (DaulWieldHolder.activeInHierarchy == false)
+        {
+            MainAmmoDisplay.SetActive(true);
+            TertararyAmmoDisplay.SetActive(false);
+        }
+    }
+}
