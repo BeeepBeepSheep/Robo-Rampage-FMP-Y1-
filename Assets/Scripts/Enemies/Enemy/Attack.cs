58c1a8be3b18251a90543fc34ec8f6a27add3306
515291 <515291@chrome.escg.ac.uk>
2021-05-06 15:25:50 +0100

    better attack

--- Assets/Scripts/Enemies/Enemy/Attack.cs
+++ Assets/Scripts/Enemies/Enemy/Attack.cs
@@ -5,49 +5,23 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     public float damage;
-    public bool attacking;
-    public Animator animator;
     public GameObject player1;
-
-    //public GameObject damageIndicator;
-    AudioSource  attackSound;
 
     public float minDamage;
     public float maxDamage;
     void Start()
     {
         player1 = GameObject.FindGameObjectWithTag("PlayerCapsual");
-        attackSound = GetComponent<AudioSource>();
 
         //damageIndicator = GameObject.FindGameObjectWithTag("DamageIndicator");
     }
     void Update()
     {
         damage = Random.Range(minDamage, maxDamage);
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_Arm_1"))
-        {
-            if (attacking)
-            {
-                return;
-            }
-            StartCoroutine(DoDamage());
-        }
     }
-    IEnumerator DoDamage()
+    public void DoDamage()
     {
-        attacking = true;
-        yield return new WaitForSeconds(.50f);
-
-        //damageIndicator.SetActive(true);
-        attackSound.Play();
-
-        yield return new WaitForSeconds(.35f);
         Health player = player1.GetComponent<Health>();
         player.TakeDamage(damage);
-        //Debug.Log("takes damage");
-
-        yield return new WaitForSeconds(.15f);
-        //damageIndicator.SetActive(false);
-        attacking = false;
     }
 }
