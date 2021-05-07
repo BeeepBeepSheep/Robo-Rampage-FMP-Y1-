c067a6cf559b2a6dc1d8e993dfe35429722dc656
515291 <515291@chrome.escg.ac.uk>
2021-05-07 09:47:33 +0100

    mid spawner rework in last commit

--- Assets/Scripts/Enemies/SpawnWaves.cs
+++ Assets/Scripts/Enemies/SpawnWaves.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-[System.Serializable]
 
 public class SpawnWaves : MonoBehaviour
 {
-    public Transform[] enemyPrefab;
+    public Transform enemyPrefab;
     private int enemyCount;
     private int maxEnemiesForWave = 1;
 
@@ -51,7 +50,7 @@ public class SpawnWaves : MonoBehaviour
         yield return new WaitForSeconds(timeBetweenWaves); //We wait here to pause between wave spawning
         for (int i = 0; i < enemiesToSpawn; i++)
         {
-            //Instantiate(enemyPrefab, spawnPoints[Random.Range(0, spawnPoints.Length)].position, enemyPrefab.transform.rotation);
+            Instantiate(enemyPrefab, spawnPoints[Random.Range(0, spawnPoints.Length)].position, enemyPrefab.transform.rotation);
             yield return new WaitForSeconds(timeBetweenEnemySpawn); //We wait here to give a bit of time between each enemy spawn
         }
         spawningWave = false;
