ba816677b3a1394a926755ca0842eb7a5fbb35eb
515291 <515291@chrome.escg.ac.uk>
2021-05-13 14:47:06 +0100

    controlls tab done

--- Assets/Scripts/UI/Settings.cs
+++ Assets/Scripts/UI/Settings.cs
@@ -4,15 +4,31 @@ using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
-    public Slider Sens_Both;
-
+    public bool promptsIsActive;
+    public GameObject prompts;
+    public GameObject promptsTick;
     void Start()
     {
-        
+        prompts.SetActive(true);
+        promptsIsActive = true;
     }
-
     void Update()
     {
         
+    }
+    public void Prompts_Toggle()
+    {
+        if(promptsIsActive)
+        {
+            prompts.SetActive(false);
+            promptsIsActive = false;
+            promptsTick.SetActive(false);
+        }
+        else
+        {
+            prompts.SetActive(true);
+            promptsIsActive = true;
+            promptsTick.SetActive(true);
+        }
     }
 }
