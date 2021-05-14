2041ca4c7384062c8878a219c3a8dc32b0cefef4
BeeepBeepSheep <515291@chrome.escg.ac.uk>
2021-05-14 19:03:59 +0100

    finished settings

--- Assets/Scripts/UI/Settings.cs
+++ Assets/Scripts/UI/Settings.cs
@@ -16,7 +16,20 @@ public class Settings : MonoBehaviour
     public Slider sensSlider;
     public Text liveSens;
 
+    public Slider masterVolSlider;
+    public Text liveMasterVol;
+    public AudioMixer masterMixer;
+    private float a;
+    private float b;
+
+    public Slider musicVolSlider;
+    public Text liveMusicVol;
+    public AudioMixer musicMixer;
+    private float c;
+    private float d;
+
     public Dropdown resolutionDropdown;
+
     Resolution[] resolutions;
     void Start()
     {
@@ -27,12 +40,12 @@ public class Settings : MonoBehaviour
         List<string> options = new List<string>();
 
         int currantResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i ++)
+        for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
 
-            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
             {
                 currantResolutionIndex = i;
             }
@@ -48,6 +61,25 @@ public class Settings : MonoBehaviour
     void Update()
     {
         liveSens.text = sensSlider.value.ToString();
+    }
+    public void Set_MasterVolume(float volume)
+    {
+        masterMixer.SetFloat("masterVolume", volume);
+
+        a = masterVolSlider.value + 80f;
+        b = masterVolSlider.maxValue + 80f;
+        liveMasterVol.text = Mathf.RoundToInt((a/b)*100).ToString() + "%";
+
+        //liveMasterVol.text = masterVolSlider.value.ToString();
+    }
+    public void Set_MusicVolume(float volume)
+    {
+        musicMixer.SetFloat("musicVol", volume);
+
+        c = musicVolSlider.value + 80f;
+        d = musicVolSlider.maxValue + 80f;
+        liveMusicVol.text = Mathf.RoundToInt((c / d) * 100).ToString() + "%";
+        //liveMusicVol.text = musicVolSlider.value.ToString();
     }
     public void Prompts_Toggle()
     {
@@ -76,14 +108,14 @@ public class Settings : MonoBehaviour
             isFullScreen = false;
             Screen.fullScreen = isFullScreen;
             fullscreenTick.SetActive(false);
-            Debug.Log("fullscreen" + isFullScreen.ToString());
+            //Debug.Log("fullscreen" + isFullScreen.ToString());
         }
         else
         {
             isFullScreen = true;
             Screen.fullScreen = isFullScreen;
             fullscreenTick.SetActive(true);
-            Debug.Log("fullscreen" + isFullScreen.ToString());
+            //Debug.Log("fullscreen" + isFullScreen.ToString());
         }
     }
 }
