661a0a22de8d0c24368b8a8bb43cc708cd848bfe
515291 <515291@chrome.escg.ac.uk>
2021-05-20 15:05:03 +0100

    main menu quit

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void LoadEasy()
+    {
+        SceneManager.LoadScene("Easy");
+    }
     public void LoadMedium()
     {
         SceneManager.LoadScene("Medium");
+    }   
+    public void LoadHard()
+    {
+        SceneManager.LoadScene("Hard");
+    }
+    public void LoadRange()
+    {
+        SceneManager.LoadScene("Range");
+    }
+    public void QuitToDesktop()
+    {
+        Application.Quit();
     }
 }
